88ecc6660a9f693669faa9c2b910bffc0c285368
Ivan Chow <ivan@ivanchow.com>
2020-05-16 17:07:01 -0400

    initial commit

---
+++ run_analysis.R
@@ -0,0 +1,30 @@
+#1 merge data
+merge_ssData <- function()
+{
+  
+}
+
+#2 extract data
+extract_data <- function()
+{
+  
+}
+
+#3 name activities
+name_activities <- function()
+{
+  
+}
+
+#4 apply labels to desriptive variable names
+apply_labels <- function()
+{
+  
+}
+
+#5 create tidy data set
+create_ssTidyData <- function()
+{
+  
+}
+
